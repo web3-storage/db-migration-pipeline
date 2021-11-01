ab5fc9ac65011bf30d0de40eb2e41249af336c49
Vasco Santos <vasco.santos@moxy.studio>
2021-11-01 14:23:34 +0100

    feat: improve validate with fetching latest fauna metrics that need to migrate (#11)

--- cmds/validate.js
+++ cmds/validate.js
@@ -10,49 +10,59 @@ const q = faunadb.query
 export async function validateCmd (opts) {
   const startTs = opts['start-ts'] ? new Date(opts['start-ts']).toISOString() :
     new Date(2010, 1, 1).toISOString()
-  let faunaMetrics
-  let endTs
-
-  if (opts['end-ts']) {
-    endTs = new Date(opts['end-ts']).toISOString()
-  }
-  console.log('end ts', endTs)
-
-  const spinnerFauna = ora('fetching fauna metrics')
-  if (!endTs) {
-    faunaMetrics = await fetchFullFaunaMetrics()
-    endTs = faunaMetrics.updated
-  } else {
-    faunaMetrics = await fetchPartialFaunaMetrics(endTs, startTs)
-  }
-  spinnerFauna.stopAndPersist()
+  let reportedFaunaMetrics
 
   const spinnerPostgres = ora('fetching postgres metrics')
-  const postgresMetrics = await fetchPostgresMetrics(endTs, startTs)
+  const postgresMetrics = await fetchPostgresMetrics(new Date().toISOString(), startTs)
   spinnerPostgres.stopAndPersist()
 
-  console.log('fauna metrics', faunaMetrics)
-  console.log('postgres metrics', postgresMetrics)
+  // Get date of last migration for partial fauna metrics
+  const latestMigrationTs = await getMigrationEndTs()
+  postgresMetrics.updated = latestMigrationTs
+
+  const spinnerReportedFauna = ora('fetching fauna reported metrics')
+  reportedFaunaMetrics = await fetchReportedFaunaMetrics()
+  const reportedEndTs = reportedFaunaMetrics.updated
+  spinnerReportedFauna.stopAndPersist()
+
+  // Get diff from Fauna
+  const spinnerDiffFauna = ora('fetching fauna diff metrics')
+  const toMigrateFaunaMetrics = await fetchPartialFaunaMetrics(reportedEndTs, latestMigrationTs)
+  spinnerDiffFauna.stopAndPersist()
+
+  const diff = {
+    users: reportedFaunaMetrics.data.users - (postgresMetrics.data.users + toMigrateFaunaMetrics.data.users),
+    cids: reportedFaunaMetrics.data.cids - (postgresMetrics.data.cids + toMigrateFaunaMetrics.data.cids),
+    uploads: reportedFaunaMetrics.data.uploads - (postgresMetrics.data.uploads + toMigrateFaunaMetrics.data.uploads),
+    pins: reportedFaunaMetrics.data.pins - (postgresMetrics.data.pins + toMigrateFaunaMetrics.data.pins),
+    contentBytes: reportedFaunaMetrics.data.contentBytes - (postgresMetrics.data.contentBytes + toMigrateFaunaMetrics.data.contentBytes)
+  }
+
+  console.log('-------------------------------------------------------------------')
+  console.log('Summary')
+  console.table([
+    { name: 'Fauna previous report', ...reportedFaunaMetrics.data, updated: reportedFaunaMetrics.updated} ,
+    { name: 'Fauna pending migr', ...toMigrateFaunaMetrics.data, updated: toMigrateFaunaMetrics.updated },
+    { name: 'Postgres migrated', ...postgresMetrics.data, updated: postgresMetrics.updated },
+    { name: 'Difference', ...diff}
+  ])
+  console.log('-------------------------------------------------------------------')
+}
+
+// Get latest timestamp of migration
+async function getMigrationEndTs () {
+  const client = await getPgClient()
+  const { rows: [{ dump_ended_at }] } = await client.query(`
+    SELECT id, dump_ended_at, MAX(id) FROM migration_tracker
+    GROUP BY id
+  `)
+
+  await client.end()
+  return new Date(dump_ended_at).toISOString()
 }
 
 async function fetchPostgresMetrics (endTs, startTs) {
-  const connectionString = getPgConnectionString()
-  const ssl = getSslState()
-
-  const client = await retry(
-    async () => {
-      const c = new Client({
-        connectionString,
-        ssl: ssl && {
-          rejectUnauthorized: false
-        }
-      })
-      await c.connect()
-      return c
-    },
-    { minTimeout: 100 }
-  )
-
+  const client = await getPgClient()
   const [
     { rows: [{ count: users }] },
     { rows: [{ count: cids }] },
@@ -79,6 +89,26 @@ async function fetchPostgresMetrics (endTs, startTs) {
       contentBytes: Number(contentBytes)
     }
   }
+}
+
+async function getPgClient () {
+  const connectionString = getPgConnectionString()
+  const ssl = getSslState()
+
+  const client = await retry(
+    async () => {
+      const c = new Client({
+        connectionString,
+        ssl: ssl && {
+          rejectUnauthorized: false
+        }
+      })
+      await c.connect()
+      return c
+    },
+    { minTimeout: 100 }
+  )
+  return client
 }
 
 async function fetchPartialFaunaMetrics (endTs, startTs) {
@@ -119,7 +149,7 @@ async function getPartialMetrics (client, query, endTs, startTs) {
   return total
 }
 
-async function fetchFullFaunaMetrics () {
+async function fetchReportedFaunaMetrics () {
   const faunaKey = getFaunaKey()
   const client = new faunadb.Client({ secret: faunaKey })
 
@@ -146,7 +176,7 @@ async function fetchFullFaunaMetrics () {
   }
 }
 
-const faunaMetricsIds = ['users_total', 'content_total', 'uploads_total', 'pins_total', 'content_bytes_total']
+const faunaMetricsIds = ['users_total_v2', 'content_total', 'uploads_total_v2', 'pins_total', 'content_bytes_total_v2']
 
 const getCountQuery = (table, endTs) => ({
   text: `
